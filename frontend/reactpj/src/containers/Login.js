286f06ffbd26a46d059456f6452b573c4e835b7a
oxo1996 <kimsm7337@gmail.com>
2020-04-17 18:05:46 +0900

    modify

--- frontend/reactpj/src/containers/Login.js
+++ frontend/reactpj/src/containers/Login.js
@@ -5,6 +5,7 @@ import { Form } from '@ant-design/compatible';
 import { connect} from 'react-redux';
 import {NavLink} from 'react-router-dom';
 import * as actions from '../store/actions/auth';
+import '@ant-design/compatible/assets/index.css';
 
 const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
@@ -35,9 +36,9 @@ class LoginForm extends React.Component{
               this.props.loading ?  
               <Spin indicator={antIcon} />
                 :
-              <Form onSubmitCapture={this.handleSubmit} className="login-form">
+              <Form onSubmit={this.handleSubmit} className="login-form">
                   <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
-                      <Input />
+                      <Input/>
                   </Form.Item>
   
                   <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
