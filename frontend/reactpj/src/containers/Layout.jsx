9294c35f050aeb489be49cb654e33e39fdce659b
Franklin Park <pss4190@naver.com>
2020-04-18 20:39:17 +0900

    승수 메인페이지 적용
    메인페이지 git으로 통합 완료

--- frontend/reactpj/src/containers/Layout.jsx
+++ frontend/reactpj/src/containers/Layout.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Avatar, Drawer, } from 'antd';
 import { Link } from 'react-router-dom';
-import { MenuOutlined } from '@ant-design/icons';
+import { MenuOutlined, UserOutlined } from '@ant-design/icons';
 import * as actions from '../store/actions/auth';
 import {connect} from 'react-redux';
+import logo from '../img/Web_logo.png';
+import '../css/MainPage.css';
 
 const { Header, Content, Footer } = Layout;
 
 class CustomLayout extends React.Component {
+    state = {
+        visible : false,
+        username : "Franklin Park",
+    };
+    
+    showDrawer = () => {
+        this.setState({
+          visible : true,
+        });
+      }
+    
+      onClose = () => {
+        this.setState({
+          visible : false,
+        });
+      }
+
     render(){
         return (
+            <div>
             <Layout className="layout">
                 <Header>
+                    <div className="image-container">
+                        <img className = "img" src={logo} alt="" />
+                    </div>
+                    <Menu theme="dark" mode="horizontal" style = {{float : 'right'}}>
+                    <Menu.Item key="1">SEARCH</Menu.Item>
+                    <Menu.Item key="2">VISION</Menu.Item>
+                    <Menu.Item key="3" onClick = {this.showDrawer}>MENU<MenuOutlined/></Menu.Item>
+                    </Menu>
+                </Header>
+                {/* <Header>
                 <div className="logo" />
                 <Menu
                     theme="dark"
@@ -30,19 +60,37 @@ class CustomLayout extends React.Component {
                         </Menu.Item>
                     }
                 </Menu>
-                </Header>
-                <Content style={{ padding: '0 50px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
+                </Header> */}
+                <Content>
+                {/* <Breadcrumb style={{ margin: '16px 0' }}>
                     <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
                     <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
                     <Breadcrumb.Item>App</Breadcrumb.Item>
-                </Breadcrumb>
-                    <div className="site-layout-content" style={{background: '#fff', padding: '24px', minHeight: '280px'}}>
+                </Breadcrumb> */}
+                    <div className="site-layout-content" style={{background: '#fff', padding : '0px', minHeight: '280px'}}>
                         {this.props.children}
                     </div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                <Footer style={{ textAlign: 'center' }}>IIF is on developing process</Footer>
             </Layout>
+            <Drawer
+            title={this.state.username}
+            placement="right"
+            closable={false}
+            onClose={this.onClose}
+            visible={this.state.visible}
+            >
+                <Avatar icon = {<UserOutlined />}>
+                <p>Username</p>
+                </Avatar>
+                <p></p>
+                <p></p>
+                <p>Arts</p>
+                <p>Status</p>
+                <p>Ranking</p>
+                <p>Arts Synthesis</p>
+            </Drawer>
+        </div>
         );
     }
 }
---
+++ frontend/reactpj/src/containers/MainPage.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Layout, Menu, Avatar, Drawer, Button, Typography } from 'antd';
+import '../css/MainPage.css';
+import temporary from '../img/Web_logo.png';
+import {
+  MenuOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const { Title, Paragraph } = Typography;
+
+const { Header, Content, Footer } = Layout;
+
+class App extends Component {
+
+  state = {
+    visible : false,
+    username : "Franklin Park",
+  };
+
+  showDrawer = () => {
+    this.setState({
+      visible : true,
+    });
+  }
+
+  onClose = () => {
+    this.setState({
+      visible : false,
+    });
+  }
+  render() {
+    return(
+      <div>
+          <div className = "content-background" alt = "">
+            <div className = "title-text" title = "contents">\
+              <Typography>
+                <Title style = {{color : "white"}}>For Better Grow of Your Arts</Title>
+                <Paragraph style = {{color : "white"}}>
+                  IIF means Iudicium In Foro, which means Evaluation of Market<br/>
+                  We provide you specific measurement for your art based on professional statics<br/>
+                </Paragraph>
+                <Paragraph style = {{color : "white"}}>
+                  Because there wasn't any flatform for Fine Arts, we decided to build this<br/>
+                  You can get evaluation from other artists and also can give them evaluation with yours
+                </Paragraph>
+              </Typography>
+              <Button type = "primary" ghost size = "large">Start with IIF</Button>
+            </div>
+          </div>
+    </div>
+    );
+  }
+}
+export default App;
