e455b5577b04d160413bb30faea771d1ba10fa4f
Franklin Park <pss4190@naver.com>
2020-04-18 22:49:14 +0900

    Temporary
    temporary

--- frontend/reactpj/src/containers/Layout.jsx
+++ frontend/reactpj/src/containers/Layout.jsx
@@ -1,39 +1,24 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Avatar, Drawer, } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import { Link } from 'react-router-dom';
+import { MenuOutlined } from '@ant-design/icons';
+import { Layout, Menu, Breadcrumb, Avatar, Drawer, } from 'antd';
+import { NavLink } from 'react-router-dom';
 import { MenuOutlined, UserOutlined } from '@ant-design/icons';
 import * as actions from '../store/actions/auth';
 import {connect} from 'react-redux';
-import logo from '../img/Web_logo.png';
-import '../css/MainPage.css';
 
 const { Header, Content, Footer } = Layout;
 
 class CustomLayout extends React.Component {
-    state = {
-        visible : false,
-        username : "Franklin Park",
-    };
-    
-    showDrawer = () => {
-        this.setState({
-          visible : true,
-        });
-      }
-    
-      onClose = () => {
-        this.setState({
-          visible : false,
-        });
-      }
-
     render(){
         return (
-            <div>
             <Layout className="layout">
                 <Header>
                     <div className="image-container">
-                        <img className = "img" src={logo} alt="" />
+                        <NavLink to={{ pathname: `/main/` }}>
+                            <img className = "img" src={logo} alt="" />
+                        </NavLink>
                     </div>
                     <Menu theme="dark" mode="horizontal" style = {{float : 'right'}}>
                     <Menu.Item key="1">SEARCH</Menu.Item>
@@ -61,36 +46,18 @@ class CustomLayout extends React.Component {
                     }
                 </Menu>
                 </Header> */}
-                <Content>
+                <Content style={{ padding: '0 50px' }}>
                 {/* <Breadcrumb style={{ margin: '16px 0' }}>
                     <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
                     <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
                     <Breadcrumb.Item>App</Breadcrumb.Item>
                 </Breadcrumb> */}
-                    <div className="site-layout-content" style={{background: '#fff', padding : '0px', minHeight: '280px'}}>
+                    <div className="site-layout-content" style={{background: '#fff', padding: '24px', minHeight: '280px'}}>
                         {this.props.children}
                     </div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>IIF is on developing process</Footer>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
             </Layout>
-            <Drawer
-            title={this.state.username}
-            placement="right"
-            closable={false}
-            onClose={this.onClose}
-            visible={this.state.visible}
-            >
-                <Avatar icon = {<UserOutlined />}>
-                <p>Username</p>
-                </Avatar>
-                <p></p>
-                <p></p>
-                <p>Arts</p>
-                <p>Status</p>
-                <p>Ranking</p>
-                <p>Arts Synthesis</p>
-            </Drawer>
-        </div>
         );
     }
 }
