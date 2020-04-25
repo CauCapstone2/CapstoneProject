a65679c39e16fe92250af5375163b6cb66bd35b5
Franklin Park <pss4190@naver.com>
2020-04-25 16:03:13 +0900

    comment
    comment for studying rest framework added

--- backend/djangopj/artifacts/api/views.py
+++ backend/djangopj/artifacts/api/views.py
@@ -8,3 +8,12 @@ class ArtifactViewSet(viewsets.ModelViewSet):
     serializer_class = ArtifactSerializer
     pagination_class = ArtifactsPagenation
     queryset = Artifact.objects.all()
+
+
+    # @action()
+    # def func() ...
+
+    # def get_queryset(self) :
+    #     qs = super().get_queryset()
+    #     qs = qs.filter(id = self.request.user)
+    #     return qs
