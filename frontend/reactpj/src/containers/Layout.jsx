aa06a170a01017b32cfeb1938bbef5ebac847f18
Franklin Park <pss4190@naver.com>
2020-05-07 18:32:02 +0900

    Merge branch 'develop' of https://github.com/CauCapstone2/CapstoneProject into develop
    # Conflicts:
    #	backend/djangopj/djangopj/settings.py
    #	backend/djangopj/djangopj/urls.py
    #	frontend/reactpj/package.json
    #	frontend/reactpj/src/containers/ArtifactDetailView.jsx

--- frontend/reactpj/src/containers/Layout.jsx
+++ frontend/reactpj/src/containers/Layout.jsx
@@ -3,7 +3,7 @@ import { Layout, Menu, Drawer, Avatar, Button, Form, Row, Col, Input } from 'ant
 import { MenuOutlined, UserOutlined } from '@ant-design/icons';
 import { NavLink } from 'react-router-dom';
 import * as actions from '../store/actions/auth';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import logo from '../img/Web_logo.png';
 import RegistrationForm from './Signup';
 import LoginForm from './Login';
@@ -12,86 +12,85 @@ const { Header, Content, Footer } = Layout;
 
 class CustomLayout extends React.Component {
     state = {
-        visible : false,
-        signupDrawer : false,
-        loginDrawer : false,
-        username : "Franklin Park",
-      };
+        visible: false,
+        signupDrawer: false,
+        loginDrawer: false,
+    };
 
-      handleSubmit = (e) => {
+    handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFieldsAndScroll((err, values) => {
-            if(!err) {
+            if (!err) {
                 this.props.onAuth(e.target.elements[0].value, e.target.elements[1].value, e.target.elements[2].value);
             }
             this.props.history.push('/');
         });
     }
 
-      showDrawer = () => {
-        this.setState({
-          visible : true,
-        });
-      }
-    
-      onClose = () => {
-        this.setState({
-          visible : false,
-        });
-      }
+    // showDrawer = () => {
+    //     this.setState({
+    //         visible: true,
+    //     });
+    // }
 
-      showSignupDrawer = () => {
+    onClose = () => {
         this.setState({
-          signupDrawer: true,
+            visible: false,
         });
-      };
-    
-      onSignupDrawerClose = () => {
-        this.setState({
-          signupDrawer: false,
-        });
-      };
+    }
 
-      showLoginDrawer = () => {
+    showSignupDrawer = () => {
         this.setState({
-          loginDrawer: true,
+            signupDrawer: true,
         });
-      };
-    
-      onLoginDrawerClose = () => {
+    };
+
+    onSignupDrawerClose = () => {
         this.setState({
-          loginDrawer: false,
+            signupDrawer: false,
         });
-      };
+    };
 
-      mainpageButtonClick = () => {
-          this.setState({
-              visible : true,
-              signupDrawer : true,
-          });
-      }
+    showLoginDrawer = () => {
+        this.setState({
+            loginDrawer: true,
+        });
+    };
 
-    render(){
+    onLoginDrawerClose = () => {
+        this.setState({
+            loginDrawer: false,
+        });
+    };
+
+    mainpageButtonClick = () => {
+        this.setState({
+            visible: true,
+            signupDrawer: true,
+        });
+    }
+
+    render() {
         return (
             <div>
-            <Layout className="layout">
-                <Header>
-                    <div className="image-container">
-                        <NavLink to={{ pathname: `/main/` }}>
-                            <img className = "img" src={logo} alt="" />
-                        </NavLink>
-                    </div>
-                    <Menu theme="dark" mode="horizontal" style = {{float : 'right'}}>
-                    <Menu.Item key="1">
-                    <NavLink to={{ pathname: `/search/` }}>
-                        SEARCH
+                <Layout className="layout">
+                    <Header>
+                        <div className="image-container">
+                            <NavLink to={{ pathname: `/main/` }}>
+                                <img className="img" src={logo} alt="" />
+                            </NavLink>
+                        </div>
+                        <Menu theme="dark" mode="horizontal" style={{ float: 'right' }}>
+                            <Menu.Item key="1">
+                                <NavLink to={{ pathname: `/search/` }}>
+                                    SEARCH
                     </NavLink>
-                    </Menu.Item>
-                    <Menu.Item key="2">VISION</Menu.Item>
-                    <Menu.Item key="3" onClick = {this.showDrawer}>MENU<MenuOutlined/></Menu.Item>
-                    </Menu>
-                </Header>
-                {/* <Header>
+                            </Menu.Item>
+                            <Menu.Item key="2">VISION</Menu.Item>
+                            <Menu.Item key="3" onClick={this.props.showDrawer}>MENU<MenuOutlined /></Menu.Item>
+                        </Menu>
+                    </Header>
+                    {/* <Header>
                 <div className="logo" />
                 <Menu
                     theme="dark"
@@ -111,68 +110,70 @@ class CustomLayout extends React.Component {
                     }
                 </Menu>
                 </Header> */}
-                <Content style = {{ minHeight : '87vh' }}>
-                {/* <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Content style={{ minHeight: '87vh' }}>
+                        {/* <Breadcrumb style={{ margin: '16px 0' }}>
                     <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
                     <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
                     <Breadcrumb.Item>App</Breadcrumb.Item>
                 </Breadcrumb> */}
-                    <div className="site-layout-content" style={{background: '#fff', padding: '0px', minHeight: '280px'}}>
-                        {this.props.children}
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>IIF is on Developing</Footer>
-            </Layout>
-            <Drawer
-            title={this.state.username}
-            placement="right"
-            closable={false}
-            onClose={this.onClose}
-            visible={this.state.visible}
-            bodyStyle={{paddingBottom : 80}}
-            footer={
-                    this.props.isAuthenticated ?
-                    <div
-                        style={{textAlign : 'right',
-                    }}>
-                        <Button onClick = {this.props.logout} style = {{ marginRight : 8}}>
-                            Logout
-                        </Button>
-                    </div>
-                    :
-                    <div
-                        style={{textAlign : 'right',
-                        }}>
-                            <Button onClick = {this.showSignupDrawer} style = {{ marginRight : 8}}>
-                                SignUp
-                            </Button>
-                            <Button onClick = {this.showLoginDrawer}type = "primary" style = {{ marginRight : 8}}>
-                                Login
-                            </Button>
-                    </div>
-            }
-            >
-                <Avatar icon = {<UserOutlined />}/>
-                <p></p>
-                <NavLink onClick = {this.onClose} to={{ pathname: `/` }}>
-                    <p>Arts</p>
-                </NavLink>
-                <NavLink onClick = {this.onClose} to={{ pathname: `/` }}>
-                    <p>Status</p>
-                </NavLink>
-                <NavLink onClick = {this.onClose} to={{ pathname: `/` }}>
-                    <p>Ranking</p>
-                </NavLink>
-                <NavLink onClick = {this.onClose} to={{ pathname: `/` }}>
-                    <p>Arts Synthesis</p>
-                </NavLink>
+                        <div className="site-layout-content" style={{ background: '#fff', padding: '0px', minHeight: '280px' }}>
+                            {this.props.children}
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>IIF is on Developing</Footer>
+                </Layout>
                 <Drawer
-                    title = "SignUp"
-                    width = {360}
-                    closable = {false}
-                    onClose = {this.onSignupDrawerClose}
-                    visible = {this.state.signupDrawer}
-                    bodyStyle = {{paddingBottom : 80}}
+                    title={this.props.username}
+                    placement="right"
+                    closable={false}
+                    onClose={this.onClose}
+                    visible={this.props.firstdrawer}
+                    bodyStyle={{ paddingBottom: 80 }}
+                    footer={
+                        this.props.isAuthenticated ?
+                            <div
+                                style={{
+                                    textAlign: 'right',
+                                }}>
+                                <Button onClick={this.props.logout} style={{ marginRight: 8 }}>
+                                    Logout
+                        </Button>
+                            </div>
+                            :
+                            <div
+                                style={{
+                                    textAlign: 'right',
+                                }}>
+                                <Button onClick={this.showSignupDrawer} style={{ marginRight: 8 }}>
+                                    SignUp
+                            </Button>
+                                <Button onClick={this.showLoginDrawer} type="primary" style={{ marginRight: 8 }}>
+                                    Login
+                            </Button>
+                            </div>
+                    }
+                >
+                    <Avatar icon={<UserOutlined />} />
+                    <p></p>
+                    <NavLink onClick={this.onClose} to={{ pathname: `/` }}>
+                        <p>Arts</p>
+                    </NavLink>
+                    <NavLink onClick={this.onClose} to={{ pathname: `/` }}>
+                        <p>Status</p>
+                    </NavLink>
+                    <NavLink onClick={this.onClose} to={{ pathname: `/` }}>
+                        <p>Ranking</p>
+                    </NavLink>
+                    <NavLink onClick={this.onClose} to={{ pathname: `/` }}>
+                        <p>Arts Synthesis</p>
+                    </NavLink>
+                    <Drawer
+                        title="SignUp"
+                        width={360}
+                        closable={false}
+                        onClose={this.onSignupDrawerClose}
+                        visible={this.state.signupDrawer}
+                        bodyStyle={{ paddingBottom: 80 }}
                     // footer = {
                     //     <div style = {{ textAlign : 'right', }}>
                     //         <Button onclick = {this.onClose} style = {{marginRight : 8}}>
@@ -185,7 +186,7 @@ class CustomLayout extends React.Component {
                     // }
                     >
                         <RegistrationForm></RegistrationForm>
-                    {/* <Form layout = "vertical" hideRequiredMark>
+                        {/* <Form layout = "vertical" hideRequiredMark>
                         <Row gutter = {16}>
                             <Col span = {12}>
                                 <Form.Item name = "username" label = "Username"
@@ -221,14 +222,14 @@ class CustomLayout extends React.Component {
                             </Col>
                         </Row>
                     </Form> */}
-                </Drawer>
-                <Drawer
-                    title = "LogIn"
-                    width = {360}
-                    closable = {false}
-                    onClose = {this.onLoginDrawerClose}
-                    visible = {this.state.loginDrawer}
-                    bodyStyle = {{paddingBottom : 80}}
+                    </Drawer>
+                    <Drawer
+                        title="LogIn"
+                        width={360}
+                        closable={false}
+                        onClose={this.onLoginDrawerClose}
+                        visible={this.state.loginDrawer}
+                        bodyStyle={{ paddingBottom: 80 }}
                     // footer = {
                     //     <div style = {{ textAlign : 'right', }}>
                     //         <Button onclick = {this.onClose} style = {{marginRight : 8}}>
@@ -241,24 +242,24 @@ class CustomLayout extends React.Component {
                     // }
                     >
                         <LoginForm />
+                    </Drawer>
                 </Drawer>
-            </Drawer>
-        </div>
+            </div>
         );
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        logout : () => dispatch(actions.logout())
+        logout: () => dispatch(actions.logout()),
+        showDrawer: ()=> dispatch(actions.firstDrawerOpen()),
     }
 }
 
 function mapReduxStateToReactProps(state) { // state가 인자인 것은 이미 규약으로 정해져 있는 것이다. redux의 store의 state이다.
     //redux의 state값이 바뀔때마다 호출되도록 규약되어있다.
     return {
-        signupDrawer : state.signupDrawer // displayNumber 's props에 들어갈 값을 여기에 적으면 되는 것이다.
+        firstdrawer : state.firstdrawer
     }
 }
-
 export default connect(mapReduxStateToReactProps, mapDispatchToProps)(CustomLayout);
