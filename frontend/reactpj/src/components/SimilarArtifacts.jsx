bd787f974411b945f57d3907e14ef9777e0e7bbf
Franklin Park <pss4190@naver.com>
2020-05-31 17:48:28 +0900

    similar creator on artifact detail with modal implementing / modal component need to be implemented.

--- frontend/reactpj/src/components/SimilarArtifacts.jsx
+++ frontend/reactpj/src/components/SimilarArtifacts.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import axios from "axios";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { Row, Col, Avatar, Typography, Divider } from "antd";
+import Profile from "../components/profile";
+import { Row, Col, Avatar, Typography, Divider, Modal } from "antd";
 
 const { Paragraph } = Typography;
 
@@ -10,6 +11,7 @@ class SimilarCreater extends React.Component {
   state = {
     similarCreater: [],
     createrInfo: [],
+    visible: false,
   };
 
   componentWillReceiveProps = (nextprops) => {
@@ -36,7 +38,7 @@ class SimilarCreater extends React.Component {
         .get("http://127.0.0.1:8000/mypage/user/?id=" + data[i])
         .then((res) => {
           console.log(res);
-          _createrInfo[i] = res.data[0].username;
+          _createrInfo[i] = res.data[0];
         });
     }
     this.setState({
@@ -44,35 +46,149 @@ class SimilarCreater extends React.Component {
     });
   };
 
+  userInformationCall = (userID) => {
+    axios.get("http://127.0.0.1:8000/mypage/user/?id=" + userID).then((res) => {
+      this.setState({
+        userInfo: res.data,
+      });
+    });
+  };
+
+  userArtifactCall = (userID) => {
+    axios
+      .get("http://127.0.0.1:8000/artifacts/api/?userID=" + userID)
+      .then((res) => {
+        this.setState({
+          artifact: res.data.results,
+        });
+        this.userEvaluationCall(res.data.results);
+      });
+  };
+
+  userCommentCall = (userID) => {
+    axios
+      .get("http://127.0.0.1:8000/mypage/comments/?userID=" + userID)
+      .then((res) => {
+        this.setState({
+          comment: res.data,
+        });
+        this.recreationErase();
+      });
+  };
+
+  userEvaluationCall = async (data) => {
+    var _evaluation = [0, 0, 0, 0, 0];
+    var _eval_num = 0;
+    for (var i in data) {
+      await axios
+        .get("http://127.0.0.1:8000/evaluation/api/?artifactID=" + data[i].id)
+        .then((res) => {
+          if (res.data[0]) {
+            _eval_num++;
+            _evaluation[0] += res.data[0].Creative;
+            _evaluation[1] += res.data[0].Expressive;
+            _evaluation[2] += res.data[0].Quality;
+            _evaluation[3] += res.data[0].Popularity;
+            _evaluation[4] += res.data[0].Workability;
+          }
+        });
+    }
+    for (var i in _evaluation) {
+      _evaluation[i] = Math.floor((_evaluation[i] * 10) / _eval_num);
+    }
+    this.setState({
+      evaluation: _evaluation,
+      _eval_length: _eval_num,
+    });
+  };
+
+  recreationErase = () => {
+    var _comments = this.state.comment;
+    var _new_comments = [];
+    for (var i in _comments) {
+      if (_comments[i].artifactID === null) {
+        continue;
+      }
+      _new_comments.push(_comments[i]);
+    }
+    this.editDate(_new_comments);
+    this.setState({
+      _new_comments: _new_comments,
+    });
+  };
+
+  editDate = (data) => {
+    for (var i in data) {
+      data[i].date = data[i].date.split(".")[0];
+      data[i].date = data[i].date.replace("T", " ");
+      data[i].date = data[i].date.replace("Z", " ");
+    }
+  };
+
+  showModal = (id) => {
+    this.userInformationCall(id);
+    this.userArtifactCall(id);
+    this.userCommentCall(id);
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOK = (e) => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = (e) => {
+    this.setState({
+      visible: false,
+    });
+  };
+
   render() {
     const { createrInfo } = this.state;
     console.log(createrInfo);
     return (
-      <Row justify="center" align="middle">
-        <Divider
-          orientation="left"
-          style={{ color: "#333", fontWeight: "normal" }}
+      <div>
+        <Row justify="center" align="middle">
+          <Divider
+            orientation="left"
+            style={{ color: "#333", fontWeight: "normal" }}
+          >
+            Similar Creators
+          </Divider>
+          {createrInfo.map((createrInfo, index) => (
+            <Col
+              key={index}
+              justify="center"
+              align="middle"
+              onClick={(createrInfo) => this.showModal(createrInfo.id)}
+            >
+              <Avatar
+                size={50}
+                style={{
+                  marginTop: "15px",
+                  marginBottom: "10px",
+                  marginLeft: "15px",
+                  marginRight: "15px",
+                }}
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+              <Typography>
+                <Paragraph>{createrInfo.username}</Paragraph>
+              </Typography>
+            </Col>
+          ))}
+        </Row>
+        <Modal
+          visible={this.state.visible}
+          onOk={this.handleOK}
+          onCancel={this.handleCancel}
         >
-          Similar Creators
-        </Divider>
-        {createrInfo.map((createrInfo, index) => (
-          <Col justify="center" align="middle">
-            <Avatar
-              size={50}
-              style={{
-                marginTop: "15px",
-                marginBottom: "10px",
-                marginLeft: "15px",
-                marginRight: "15px",
-              }}
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-            />
-            <Typography>
-              <Paragraph>{createrInfo}</Paragraph>
-            </Typography>
-          </Col>
-        ))}
-      </Row>
+          <Profile />
+        </Modal>
+      </div>
     );
   }
 }
