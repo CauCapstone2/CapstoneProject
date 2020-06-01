03e30889c5d461afb83bb6eedfb9485f10277ad9
Franklin Park <pss4190@naver.com>
2020-06-01 20:16:18 +0900

    Kakao api imported to react project

--- frontend/reactpj/src/containers/MainPage.jsx
+++ frontend/reactpj/src/containers/MainPage.jsx
@@ -12,6 +12,14 @@ class MainPage extends Component {
     this.props.signupDrawerOpen();
   };
 
+  // componentDidMount() {
+  //   window.Kakao.init("API키 입력");
+  //   window.Kakao.PlusFriend.createAddFriendButton({
+  //     container: "#plusfriend-addfriend-button",
+  //     plusFriendId: "_xcLqmC", // 플러스친구 홈 URL에 명시된 id로 설정합니다.
+  //   });
+  // };
+
   render() {
     return (
       <div>
