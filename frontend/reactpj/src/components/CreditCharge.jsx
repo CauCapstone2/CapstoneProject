cc6159662ca18b88ec3052692e9ad947f29a8188
Franklin Park <pss4190@naver.com>
2020-06-07 16:23:35 +0900

    KakaoPay API 1/3 step implemented.

--- frontend/reactpj/src/components/CreditCharge.jsx
+++ frontend/reactpj/src/components/CreditCharge.jsx
@@ -27,27 +27,85 @@ class CreditCharge extends React.Component {
   handleCharge = () => {
     console.log("in handleCharge" + this.state.creditAmount);
     let price_for_credit = this.state.creditAmount;
-    let form_data = new FormData();
-    form_data.append("cid", "TC0ONETIME");
-    form_data.append("partner_order_id", "partner_order_id");
-    form_data.append("partner_user_id", "partner_user_id");
-    form_data.append("item_name", `credit_purchase_${price_for_credit}`);
-    form_data.append("quantity", 1);
-    form_data.append("total_amount", price_for_credit);
-    form_data.append("tax_free_amount", 0);
-    form_data.append("approval_url", "http://localhost:3000/mypage");
-    form_data.append("fail_url", "http://localhost:3000/mypage");
-    form_data.append("cancel_url", "http://localhost:3000/mypage");
+
+    // var requestBody = {
+    //   "cid": "TC0ONETIME",
+    //   "partner_order_id": "partner_order_id",
+    //   "partner_user_id": "partner_user_id",
+    //   "item_name": `credit_purchase_${price_for_credit}`,
+    //   "quantity": 1,
+    //   "total_amount": price_for_credit,
+    //   "tax_free_amount": 0,
+    //   "approval_url": "http://localhost:3000/mypage",
+    //   "fail_url": "http://localhost:3000/mypage",
+    //   "cancel_url": "http://localhost:3000/mypage",
+    // };
+
+    const params = new URLSearchParams();
+    params.append("cid", "TC0ONETIME");
+    params.append("partner_order_id", "partner_order_id");
+    params.append("partner_user_id", "partner_user_id");
+    params.append("item_name", "credit_purchase");
+    params.append("quantity", 1);
+    params.append("total_amount", price_for_credit);
+    params.append("tax_free_amount", 0);
+    params.append("approval_url", "http://localhost:3000/mypage");
+    params.append("fail_url", "http://localhost:3000/mypage");
+    params.append("cancel_url", "http://localhost:3000/mypage");
+
+    // var sending_data = [];
+    // for (var property in requestBody) {
+    //     var encodedKey = encodeURIComponent(property);
+    //     var encodedValue = encodeURIComponent(requestBody[property]);
+    //     sending_data.push(encodedKey + "=" + encodedValue);
+    // }
+    // sending_data = sending_data.join("\\");
+
+    // console.log(sending_data);
+    
+    // var form_data = new FormData();
+    // form_data.append("cid", "TC0ONETIME");
+    // form_data.append("partner_order_id", "partner_order_id");
+    // form_data.append("partner_user_id", "partner_user_id");
+    // form_data.append("item_name", "credit_purchase");
+    // form_data.append("quantity", 1);
+    // form_data.append("total_amount", price_for_credit);
+    // form_data.append("tax_free_amount", 0);
+    // form_data.append("approval_url", "http://localhost:3000/mypage");
+    // form_data.append("fail_url", "http://localhost:3000/mypage");
+    // form_data.append("cancel_url", "http://localhost:3000/mypage");
     axios
-      .post("https://kapi.kakao.com/v1/payment/ready", form_data, {
-        headers: {
-          "Authorization": `KaKaoAK ${keys.kakao_admin_key}`,
-        },
-      })
+      .post(
+        "https://cors-anywhere.herokuapp.com/https://kapi.kakao.com/v1/payment/ready",
+        params,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+            "Authorization": `KakaoAK ${keys.kakao_admin_key}`,
+            // "Accept": "application/json;charset=UTF-8"
+          },
+        }
+      )
       .then((res) => {
-          console.log(res);
+        console.log("YE");
+        console.log(res);
+      })
+      .catch((res) => {
+        console.log(res);
       });
   };
+
+//   handleCharge_sub = () => {
+//     Kakao.API.request({
+//       url: "/v1/payment/ready",
+//       success: function (res) {
+//         alert(JSON.stringify(res));
+//       },
+//       fail: function (error) {
+//         alert(JSON.stringify(error));
+//       },
+//     });
+//   };
 
   handleCancel = () => {};
 
