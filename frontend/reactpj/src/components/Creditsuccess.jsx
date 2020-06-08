17c02cccc2eb99cf0fd0e735fa8fce96b921f891
pss4190 <pss4190@naver.com>
2020-06-08 16:21:31 +0900

    purchase system via kakaoAPI develop done.

--- frontend/reactpj/src/components/Creditsuccess.jsx
+++ frontend/reactpj/src/components/Creditsuccess.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import axios from "axios";
 import { Result, Button } from "antd";
 import { connect } from "react-redux";
+import * as actions from "../store/actions/auth";
 import * as keys from "./kakaosecurityinfo";
+import { NavLink } from "react-router-dom";
 
 class Creditsuccess extends React.Component {
+  state = {
+    purchase_done: false,
+  };
+  componentWillMount() {
+    this.props.tid_check();
+  }
   handleCharge = () => {
     var not_modified_pg = this.props.location.search;
     var pg_token = not_modified_pg.substr(10);
@@ -18,14 +26,6 @@ class Creditsuccess extends React.Component {
     params.append("tid", this.props.tid);
     params.append("pg_token", pg_token);
 
-    // curl -v -X POST 'https://kapi.kakao.com/v1/payment/approve' \
-    // -H 'Authorization: KakaoAK xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' \
-    // --data-urlencode 'cid=TC0ONETIME' \
-    // --data-urlencode 'tid=T1234567890123456789' \
-    // --data-urlencode 'partner_order_id=partner_order_id' \
-    // --data-urlencode 'partner_user_id=partner_user_id' \
-    // --data-urlencode 'pg_token=xxxxxxxxxxxxxxxxxxxx'
-
     axios
       .post(
         "https://cors-anywhere.herokuapp.com/https://kapi.kakao.com/v1/payment/approve",
@@ -38,8 +38,12 @@ class Creditsuccess extends React.Component {
         }
       )
       .then((res) => {
-          console.log("this.props : " + this.props);
-          console.log("success");
+        console.log("this.props : " + this.props);
+        console.log("success");
+        this.props.tid_delete();
+        this.setState({
+          purchase_done: true,
+        });
       })
       .catch((res) => {
         console.log(res);
@@ -50,19 +54,42 @@ class Creditsuccess extends React.Component {
     console.log(this.props);
     console.log("search : " + this.props.location.search);
     return (
-      <Result
-        status="success"
-        title="Successfully Purchased Credit for using IIF"
-        subTitle="You can use credits at downloading artifacts or other services"
-        extra={[
-          <Button type="primary" key="done" onClick={this.handleCharge}>
-            Go Mainpage
-          </Button>,
-        ]}
-      />
+      <div>
+        {this.state.purchase_done ? (
+          <Result
+            status="success"
+            title="Your Purchase is Authorized."
+            subTitle="Thank you for purchasing our credit. It will help creators thanks to you."
+            extra={
+              <NavLink to={{ pathname: "/mypage" }}>
+                <Button type="primary" key="success">
+                  Back to Mypage
+                </Button>
+              </NavLink>
+            }
+          />
+        ) : (
+          <Result
+            title="Your Purchase is right before to be completed."
+            subTitle="Click the button below to complete your purchase."
+            extra={
+              <Button type="primary" key="done" onClick={this.handleCharge}>
+                Confirm Purchase
+              </Button>
+            }
+          />
+        )}
+      </div>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    tid_delete: () => dispatch(actions.tidDataDelete()),
+    tid_check: () => dispatch(actions.tidDataCheck()),
+  };
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -70,4 +97,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Creditsuccess);
+export default connect(mapStateToProps, mapDispatchToProps)(Creditsuccess);
