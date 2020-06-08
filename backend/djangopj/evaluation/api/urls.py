5785819e030fd353d13c21b3885e6295c42f903e
성현 <seong5016@gmail.com>
2020-06-08 12:15:40 +0900

    evaluation 평균값 구하는 백엔드 구현 완료

--- backend/djangopj/evaluation/api/urls.py
+++ backend/djangopj/evaluation/api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from evaluation.api.views import EvaluationViewSet
 from rest_framework.routers import DefaultRouter
+from django.conf.urls import url
+from django.urls import path, include
+from .views import EvaluationView
+
 
 router = DefaultRouter()
 router.register(r'', EvaluationViewSet, basename='evaluation')
-urlpatterns = router.urls
+
+urlpatterns = [
+    url(r'^average/$', EvaluationView.as_view(), name='evaluation average'),
+    url(r'', include(router.urls))
+]
--- backend/djangopj/evaluation/api/views.py
+++ backend/djangopj/evaluation/api/views.py
@@ -1,7 +1,10 @@
 from evaluation.models import Evaluation
 from .serializers import EvaluationDetailSerializer
 from rest_framework import viewsets
+from rest_framework.views import APIView
 from django_filters import rest_framework as filters
+from ..models import Evaluation
+from rest_framework.response import Response
 
 
 class EvaluationViewSet(viewsets.ModelViewSet):
@@ -9,3 +12,24 @@ class EvaluationViewSet(viewsets.ModelViewSet):
     queryset = Evaluation.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ('artifactID', 'userID',)
+
+
+class EvaluationView(APIView):
+    def get(self, request):
+        artifactId = int(request.GET.get('artifactID'))
+        evaluations = Evaluation.objects.filter(artifactID=artifactId)
+        print(evaluations.values_list(flat=True))
+        print(evaluations.values())
+
+        average = [0, 0, 0, 0, 0]
+        for eval in evaluations:
+            average[0] += eval.Creative
+            average[1] += eval.Expressive
+            average[2] += eval.Quality
+            average[3] += eval.Popularity
+            average[4] += eval.Workability
+        average = [x / len(evaluations) for x in average]
+
+        result = {'average': average, 'length': len(evaluations)}
+
+        return Response(result)
