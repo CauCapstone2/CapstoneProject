bc9b4c8b15fd787fdda03f6868f0bbe23d6c6994
pss4190 <pss4190@naver.com>
2020-06-09 15:25:19 +0900

    credit backend progressing.

---
+++ backend/djangopj/credit/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.generics import UpdateAPIView
+from rest_framework.views import APIView
+
+# class CreditViewSet(viewset.MOdelViewSet) :
+#     serializer_class
+
+# class CreditUpdateView(APIView) :
+#     def post(self, request, format=None) :
--- frontend/reactpj/src/components/CreditCharge.jsx
+++ frontend/reactpj/src/components/CreditCharge.jsx
@@ -157,20 +157,3 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreditCharge);
-
-// import { Result, Button } from 'antd';
-
-// ReactDOM.render(
-//   <Result
-//     status="success"
-//     title="Successfully Purchased Cloud Server ECS!"
-//     subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
-//     extra={[
-//       <Button type="primary" key="console">
-//         Go Console
-//       </Button>,
-//       <Button key="buy">Buy Again</Button>,
-//     ]}
-//   />,
-//   mountNode,
-// );
